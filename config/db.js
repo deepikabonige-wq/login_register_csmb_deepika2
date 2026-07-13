851d2540aa980908bc47661c366d95961c9c5ee7
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-13 22:46:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const dns = require("dns")
+const dns = require("dns");
 
-dns.setServers(["8.8.8.8","8.8.4.4"])
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
-  
-    const connString = 'mongodb+srv://KulsumShaik24_db_user:<kulsum20>@cluster0.nxftcfq.mongodb.net/kulsum?appname=cluster0';
+    // MongoDB Atlas connection string
+    const connString = "mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0";
     
     await mongoose.connect(connString);
-    console.log('MongoDB connected successfully to LOCALHOST!');
+    console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('Database connection error:', err.message);
     process.exit(1);
