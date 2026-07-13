0a00fef4f0880ee4d7ed11247c57b7d4ad9488fc
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-13 14:26:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const dns = requrie("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 
 const connectDB = async () => {
   try {
     
-    await mongoose.connect('mongodb://localhost:27017/workshop_db');
+    await mongoose.connect('mongodb+srv://KulsumShaik24_db_user:<db_password>@cluster0.nxftcfq.mongodb.net/kulsum?appname=cluster0');
     
     console.log('MongoDB connected successfully!');
   } catch (err) {
