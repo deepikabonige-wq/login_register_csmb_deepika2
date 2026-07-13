b2104e3eba3b3ad8d17d236a6b1c4eb0cd799892
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-13 21:51:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const dns = require("dns")
-dns.setServers(["8.8.8.8","8.8.4.4"])
 
 const connectDB = async () => {
   try {
+  
+    const connString = "mongodb://localhost:27017/workshop_db";
     
-    await mongoose.connect('mongodb+srv://KulsumShaik24_db_user:<kulsum@20>@cluster0.nxftcfq.mongodb.net/kulsum?appname=cluster0');
-    
-    console.log('MongoDB connected successfully!');
+    await mongoose.connect(connString);
+    console.log('MongoDB connected successfully to LOCALHOST!');
   } catch (err) {
     console.error('Database connection error:', err.message);
     process.exit(1);
